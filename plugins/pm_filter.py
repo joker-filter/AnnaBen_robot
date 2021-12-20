5cfa92657096647ea17e10ea442c4d9f0157c8b0
joker-filter <96322951+joker-filter@users.noreply.github.com>
2021-12-20 07:56:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -452,7 +452,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.NEXT_TXT.format(query.from_user.mention),
+            text=script.HELP_TXT.format(query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
